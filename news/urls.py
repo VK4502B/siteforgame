dd8b1e3af5b3abd31a4ea6ddaac50586b5f972c4
Rostyslav Kozlivskij <bibbba244@gmail.com>
2017-04-10 23:23:02 +0300

    add article detail

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from news.views import ArticleListView
 from news.views import article_create
+from news.views import ArticleDetailView
 from . import views
 
 
 urlpatterns = [
     url(r'^$', ArticleListView.as_view(), name='article-list'),
+    url(r'^(?P<slug>[-\w]+)/$', ArticleDetailView.as_view(), name='article-detail'),
     url(r'^create/$', article_create, name='create'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.utils import timezone
 from django.http import HttpResponse
+
 from news.models import Article
 from .forms import ArticleForm
 
@@ -13,6 +15,16 @@ class ArticleListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ArticleListView, self).get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+
+class ArticleDetailView(DetailView):
+
+    model = Article
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
 
